c2b9427a4f0388b2494846a67ed118e43f8fb06e
Sona <sonali23.1293@gmail.com>
2021-07-24 21:14:04 -0400

    delete data

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,25 +2,41 @@ const router = require("express").Router();
 const {filterByQuery, createNewNote, validateNote} = require("../../lib/notes");
 const { notes } = require("../../data/notes.json");
 
+const fs = require("fs");
+const path = require("path");
+
 router.get("/notes", (req, res) => {
-    // let results = notes;
-    // if(req.query){
-    //     results = filterByQuery(req.query, results);
-    // };
-    // res.json(results);
     res.json(notes);
 });
 
 router.post("/notes", (req, res) => {
 
-    req.body.id = (notes.length+1).toString();
-  
+  req.body.id = (notes.length+1).toString();
     if(!validateNote(req.body)){
         res.status(400).send("Please add Note Title.");
     }else{
+    
     const note = createNewNote(req.body, notes);
     res.json(note);
     }
+});
+
+router.delete("/notes/:id", (req, res) => {
+
+    const index = req.params.id;
+   
+    if (index === -1) return res.status(404).json({})
+   
+    notes.splice(index-1, 1);
+
+    notes.forEach((note, index) => note.id = (index+1).toString());
+
+    fs.writeFileSync(
+        path.join(__dirname, "../../data/notes.json"),
+        JSON.stringify({ notes }, null, 2)
+      );
+
+      res.json(notes);
 });
 
 module.exports = router;
