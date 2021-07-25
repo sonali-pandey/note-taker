4b53b34e3b4d5694db3a91a087f83a1fe732dc78
Sona <sonali23.1293@gmail.com>
2021-07-24 21:53:03 -0400

    comment and demo

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
-const {filterByQuery, createNewNote, validateNote} = require("../../lib/notes");
+const {createNewNote, validateNote} = require("../../lib/notes");
 const { notes } = require("../../data/notes.json");
 
 const fs = require("fs");
 const path = require("path");
 
+// getting the existing notes
 router.get("/notes", (req, res) => {
     res.json(notes);
 });
 
+// adding a new note
 router.post("/notes", (req, res) => {
 
   req.body.id = (notes.length+1).toString();
@@ -21,16 +23,22 @@ router.post("/notes", (req, res) => {
     }
 });
 
+
+// delete a note
 router.delete("/notes/:id", (req, res) => {
 
+    // getting index of the note to be deleted
     const index = req.params.id;
    
     if (index === -1) return res.status(404).json({})
    
+    // removing the note from the notes array
     notes.splice(index-1, 1);
 
+    // updating the existing notes's id
     notes.forEach((note, index) => note.id = (index+1).toString());
 
+    // writing the new notes array to the database json file
     fs.writeFileSync(
         path.join(__dirname, "../../data/notes.json"),
         JSON.stringify({ notes }, null, 2)
